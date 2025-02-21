98c36de8ecbdd2b75b729703aaa1d5759f404495
kwt <122835363+kwt@users.noreply.github.com>
2025-02-21 19:16:39 +0000

    default vehicles
    - simeons
    - jobs
    - nhs
    - police
    - other (taxi, bus, casino car, g4s etc)

---
+++ fxmanifest.lua
@@ -0,0 +1,32 @@
+fx_version 'cerulean'
+games { 'rdr3', 'gta5' }
+
+files {
+    'data/**/handling.meta',
+    'data/**/vehiclelayouts.meta',
+    'data/**/vehicles.meta',
+    'data/**/carvariations.meta',
+    'data/**/carcols*.meta',
+    'els/vcfs.json', 
+    'els/vcf/*.xml',
+}
+
+data_file 'HANDLING_FILE' 'data/**/handling.meta'
+data_file 'VEHICLE_LAYOUTS_FILE' 'data/**/vehiclelayouts.meta'
+data_file 'VEHICLE_METADATA_FILE' 'data/**/vehicles.meta'
+data_file 'CARCOLS_FILE' 'data/**/carcols*.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'data/**/carvariations.meta'
+
+
+client_scripts {
+    "garages.lua",
+    "vehicle_names.lua",
+    "cfg_vehiclemaxspeeds.lua",
+}
+
+shared_scripts {
+    "inventory.lua",
+    "garages.lua",
+    "cfg_vehiclemaxspeeds.lua",
+}
+
